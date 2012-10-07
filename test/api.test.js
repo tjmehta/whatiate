5a1b0baf072e9c99eef2a032ce3609b814e432ac
Tejesh Mehta <tj@init.me>
2012-10-07 15:17:28 -0700

    phone loging route

--- test/api.test.js
+++ test/api.test.js
@@ -107,58 +107,60 @@ describe("Tests:", function(){
           });
           it("should return we logged", function(){
             should.exist(loginMessage2.match(/log/));
-          })
+          });
         });//login
 
-        describe("SMSGetRecommendations:", function(){
-          var loginError2, loginMessage2;
-          var messageIn2 = "recommend"
+        // describe("SMSGetRecommendations:", function(){
+        //   var loginError2, loginMessage2;
+        //   var messageIn2 = "recommend"
+        //   before(function(done){
+        //     api.users.login(cookies, phoneNumber, messageIn2, function(error, textMessage){
+        //       loginError2 = error;
+        //       loginMessage2 = textMessage;
+        //       done();
+        //     });
+        //   });
+        //   it("should NOT return an error", function(){
+        //     should.not.exist(loginError);
+        //   });
+        //   it("should return a text message", function(){
+        //     console.log(loginMessage2);
+        //     should.exist(loginMessage2);
+        //   });
+        //   it("should NOT return Welcome", function(){
+        //     should.not.exist(loginMessage2.match(/elcome/));
+        //   });
+        //   it("should return list", function(){
+        //     should.exist(loginMessage2.match(/\(/));
+        //   })
+        // });//login
+
+        describe("getRecent:", function(){
+          var recentError, recentStuff;
+          var messageIn2 = "banana";
           before(function(done){
             api.users.login(cookies, phoneNumber, messageIn2, function(error, textMessage){
-              loginError2 = error;
-              loginMessage2 = textMessage;
-              done();
-            });
-          });
-          it("should NOT return an error", function(){
-            should.not.exist(loginError);
-          });
-          it("should return a text message", function(){
-            console.log(loginMessage2);
-            should.exist(loginMessage2);
-          });
-          it("should NOT return Welcome", function(){
-            should.not.exist(loginMessage2.match(/elcome/));
-          });
-          it("should return link", function(){
-            should.exist(loginMessage2.match(/http/));
-          })
-        });//login
+              recentError = error;
+              messageIn2 = "mango";
+              api.users.login(cookies, phoneNumber, messageIn2, function(error, textMessage){
+                loginError2 = error;
+                loginMessage2 = textMessage;
 
+                var userId = cookies.get('userId');
+                api.food.getRecent(userId, function(error, recentEats){
 
-
-        describe("Food:", function(){
-          describe("Recommendations:", function(){
-            var recError, recList;
-            before(function(done){
-              api.food.getRecommendations(phoneNumber, function(error, recommendations){
-                recError = error;
-                recList = recommendations;
+                });
                 done();
               });
             });
           });
-          describe("Log:", function(){
-            var foodName = "chicken burrito";
-            var logError, logEntry;
-            before(function(done){
-              api.food.log(phoneNumber, foodName, function(error, ate){
-                logError = error;
-                logEntry = ate;
-              });
-            });
+          it("should NOT return an error", function(){
+            should.not.exist(loginError2);
+            if (loginError2) console.log(loginError2);
           });
-        });
+        });//login
+
+
     });//smsRegister
   });
 });
